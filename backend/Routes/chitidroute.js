954e34b5fdaa090768bcf87efa27a102eac20606
vivinlara <laravivin15uco@gmail.com>
2025-06-12 18:22:19 +0530

    mango1

--- backend/Routes/chitidroute.js
+++ backend/Routes/chitidroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const chitIdController = require('../Controllers/chitidController');
+const chitIdController = require('../Controllers/ChitidController');
 
 // POST /api/chitids
 router.post('/', chitIdController.createChitId);

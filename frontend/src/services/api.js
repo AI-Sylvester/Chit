0bc438d9cadacb929683ff41a3dc6f700ff65c7a
vivinlara <laravivin15uco@gmail.com>
2025-06-18 14:04:31 +0530

    net

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-// Axios instance for local backend
-export const localAPI = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: { 'Content-Type': 'application/json' },
-});
-
-// Axios instance for deployed backend
-export const renderAPI = axios.create({
-  baseURL: 'https://my-ecom-hdyc.onrender.com/api',
-  headers: { 'Content-Type': 'application/json' },
+const api = axios.create({
+  baseURL:
+    'http://localhost:5000/api' || 'https://my-ecom-hdyc.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
